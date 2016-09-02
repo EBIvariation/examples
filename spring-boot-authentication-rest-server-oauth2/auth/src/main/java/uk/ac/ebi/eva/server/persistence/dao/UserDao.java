248213c1ab4c4ad7255babdbafa67f63199c1708
jorizci <jorizci@gmail.com>
2016-09-02 11:24:57 +0100

    Fixed wrong structure in the oauth2 server

--- spring-boot-authentication-rest-server-oauth2/auth/src/main/java/uk/ac/ebi/eva/server/persistence/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.eva.server.persistence.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
-import uk.ac.ebi.eva.server.persistence.entities.User;
-
-@Transactional
-public interface UserDao extends CrudRepository<User, String> {
-}
