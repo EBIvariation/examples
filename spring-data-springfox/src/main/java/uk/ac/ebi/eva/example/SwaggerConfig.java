b033382a3019f53c939fc75d92be55420ace9f0c
jorizci <jorizci@gmail.com>
2018-04-27 00:21:02 +0100

    Added springfox

---
+++ spring-data-springfox/src/main/java/uk/ac/ebi/eva/example/SwaggerConfig.java
@@ -0,0 +1,136 @@
+/*
+ *
+ * Copyright 2018 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package uk.ac.ebi.eva.example;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.AlternateTypeRule;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.WildcardType;
+import java.util.Arrays;
+import java.util.List;
+
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+@Configuration
+@EnableSwagger2
+@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
+public class SwaggerConfig {
+
+    @Autowired
+    private TypeResolver typeResolver;
+
+    @Bean
+    public Docket metadataApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                //.paths(getScanRestServicesPathPredicate())
+                .build()
+                .apiInfo(getApiInfo())
+                .pathMapping("/")
+                .tags(
+                        new Tag("File Entity", "Some definition")
+                )
+                //.directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(getSubstitutionRules());
+    }
+
+    private List<ResponseMessage> getResponseMessagesForPostAndPatch() {
+        return Arrays.asList(new ResponseMessageBuilder()
+                        .code(201)
+                        .message("Created")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(400)
+                        .message("Validation error")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(401)
+                        .message("Unauthorized")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(403)
+                        .message("Forbidden")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(404)
+                        .message("Not Found")
+                        .build());
+    }
+
+    private Predicate<String> getScanRestServicesPathPredicate() {
+        return Predicates.and(
+                Predicates.not(PathSelectors.regex("/actuator.*")), // Hide spring-actuator
+                Predicates.not(PathSelectors.regex("/error.*")), // Hide spring-data error
+                Predicates.not(PathSelectors.regex("/profile.*")) // Hide spring-data profile
+        );
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .contact(new Contact("EMBL-EBI", "https://www.ebi.ac.uk", null))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .termsOfServiceUrl(null)
+                .title("Workshop")
+                .description("Spring data rest - springfox")
+                .version("1.0")
+                .build();
+    }
+
+    private AlternateTypeRule getSubstitutionRules() {
+        return newRule(typeResolver.resolve(DeferredResult.class,
+                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                typeResolver.resolve(WildcardType.class));
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayRequestDuration(true)
+                .filter(false)
+                .validatorUrl(null)
+                .build();
+    }
+
+}
